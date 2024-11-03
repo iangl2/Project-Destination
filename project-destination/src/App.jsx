0e424124ac6d13c08c3a01fd487855c04a21d699
iangl2 <93356017+iangl2@users.noreply.github.com>
2024-11-03 01:48:13 -0500

    This commit I merged the changes from the branch 'Ian' and Branch Edgar into the branch 'main'

--- project-destination/src/App.jsx
+++ project-destination/src/App.jsx
@@ -1,26 +1,18 @@
 
 import './App.css'
-<<<<<<< HEAD
-import Footer from './components/Footer'
-
-=======
 import Navbar from './components/navbar';
->>>>>>> Ian-Branch
 
 
 function App() {
   
   return (
     <>
-<<<<<<< HEAD
     
       
       <Footer/>
-=======
     <Navbar>
       
     </Navbar>
->>>>>>> Ian-Branch
     </>
   )
 }
