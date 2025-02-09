adfd515f6213ab763b8f036de35272ee820ec718
EdgarDaza <edgar.daza@utp.ac.pa>
2025-02-09 00:03:24 -0500

    Agregado PlacesPlus

---
+++ project-destination/src/pages/PlacesPlus/placesplus.jsx
@@ -0,0 +1,11 @@
+import './styles/placesplus.css';
+
+const PlacesPlus = () => {
+    return (
+        <div className='placesplus-container'>
+            <h1>Places Plus</h1>
+        </div>
+    );
+}
+
+export default PlacesPlus;
